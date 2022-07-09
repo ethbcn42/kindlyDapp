f4205ae97250264b24482ecdb554c5c77a2cbeac
javier cervilla <javier@MacBook-Pro-de-javier.local>
2022-07-09 14:01:53 +0200

    feat(Landing):[+]Add landing before merge

--- components/Web3/ConnectButton.jsx
+++ components/Web3/ConnectButton.jsx
@@ -4,8 +4,8 @@ import { ConnectButton } from "web3uikit"
 export default function ConnectButtonWeb3() {
     return (
         <ConnectButton
-        chainId={network.chainId}
-        signingMessage={`I will sign this automatic generated ID to prove that i am the owner of this wallet.`}
+            chainId={network.chainId}
+            signingMessage={`I will sign this automatic generated ID to prove that i am the owner of this wallet.`}
         />
     )
 }
