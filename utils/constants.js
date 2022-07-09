6d557c73d66d4e7643c805665b4c3276edc7e294
javier cervilla <javier@MacBook-Pro-de-javier.local>
2022-07-09 21:59:56 +0200

    feat(useContract): [+] Use contract Hook connected

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,10 @@
 export const network = {
     name: "Göerli",
-    chainId: 5,
-    contract: ""
+    chainId: "0x5",
+    contract: {
+        addy: "0xa44C954C54Ff2E08e704b48981B5CEc4e50590d9",
+        abi: [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint8","name":"version","type":"uint8"}],"name":"Initialized","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"Registry","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_percent","type":"uint256"},{"internalType":"address","name":"_ong","type":"address"},{"internalType":"address","name":"_wallet","type":"address"}],"name":"createParticular","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_admin","type":"address"},{"internalType":"bytes32","name":"_seed","type":"bytes32"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"seed","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_admin","type":"address"}],"name":"setAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_seed","type":"bytes32"}],"name":"setSeed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32[]","name":"_merkleProof","type":"bytes32[]"}],"name":"verifyProof","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
+    }
 }
 
 
