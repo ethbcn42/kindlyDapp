469202244a17afb844f3642dd25d862dc699abc7
javier cervilla <javier@MacBook-Pro-de-javier.local>
2022-07-10 05:36:00 +0200

    fix(SEO): [+]Remove Universelle Subliminal Publicity 🤣

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 const SEO = ({
     title = 'Kindly',
     description = 'Kindly its a free, friendly & open-source platform that automatizes donations to non-profit organizations, which allows NFT creators to deduct taxes.',
-    images = [{ url: "/logo.svg" }],
+    images = [{ url: "https://kindly-dapp.vercel.app/logo.png" }],
     url = "https://kindly-dapp.vercel.app",
 }) => {
     return (
@@ -12,7 +12,7 @@ const SEO = ({
             <title>{title}</title>
             <meta name="description" content={description} key="description" />
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@universelle_io" />
+            <meta name="twitter:site" content="@kindlyweb3" />
             <meta name="twitter:title" content={title} key="tw-title" />
             <meta name="twitter:description" content={description} key="tw-description" />
             <meta name="twitter:image" content={images[0].url} key="tw-images" />
@@ -24,7 +24,7 @@ const SEO = ({
             <meta property="og:image" content={images[0].url} key="og-images" />
             <meta property="og:url" content={url} />
             <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Universelle" />
+            <meta property="og:site_name" content={title} />
             <meta property="og:locale" content="es_ES" />
         </Head>
     )
